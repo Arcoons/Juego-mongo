a5aed2c365e7ba610ddf8f16675198b1a9c3535b
Christian <christianzaor1090@gmail.com>
2022-12-03 20:33:05 -0500

    intento 2

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const open = ()=>{
-    const comandos = require('child_process')
-
-    comandos.spawn('cmd',['/c', 'start c:\\mongosh-1.6.0-win32-x64\\bin\\mongosh'])
+function cons(){
+    var objShell = new ActiveXObject("Shell.Application");
+    objShell.ShellExecute("mongosh.exe", "C: cd C:\\pr main.exe blablafile.txt auto", "C:\\mongosh-1.6.0-win32-x64\\bin", "open", "1");
 }
+ 
+//NOTE: ONLY WORKS ON WINDOWS AS I KNOW OF
+
+//var {spawn}=require('child_process')
+
+//function cons(){
+ //   let temporal =spawn('cmd',['/c', 'start c:\\mongosh-1.6.0-win32-x64\\bin\\mongosh'])
+
+//}
+
+
+    
